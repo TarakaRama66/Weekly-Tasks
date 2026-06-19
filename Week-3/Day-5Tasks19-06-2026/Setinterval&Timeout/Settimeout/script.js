1059828a298052dd660d56d38b21cd3a92ccd393
Taraka rama tilak gupta <tarakarama.tilak@nybinfotech.com>
2026-06-19 17:01:53 +0530

    19-06-2026

---
+++ Week-3/Day-5Tasks19-06-2026/Setinterval&Timeout/Settimeout/script.js
@@ -0,0 +1,27 @@
+console.log("Welcome to Zomato Delivery App");
+
+console.log(" Your Order Placed Successfully");
+
+setTimeout(() => {
+    console.log("Accepted Your Order");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Started Food Preparation ");
+}, 4000);
+
+setTimeout(() => {
+    console.log("Completed");
+}, 7000);
+
+setTimeout(() => {
+    console.log("Delivery Partner Assigned Your Order");
+}, 9000);
+
+setTimeout(() => {
+    console.log("Order Picked Up");
+}, 12000);
+
+setTimeout(() => {
+    console.log("Order Delivered Successfully");
+}, 15000);
