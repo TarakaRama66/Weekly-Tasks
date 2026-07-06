ad482a0f8b6efcf4fdb2fd6c4747cc4c6f066861
Taraka rama tilak gupta <tarakarama.tilak@nybinfotech.com>
2026-07-06 18:00:49 +0530

    06-07-2026

---
+++ Week-6/react/src/Functional Components/Personalinformation/Details.jsx
@@ -0,0 +1,16 @@
+function Details() {
+    const name = "Tarak";
+    const age = 23;
+    const course = "Frontend Developer";
+    const city = "Hyderabad";
+    return(
+        <div>
+            <h1>Personal Information</h1>
+            <p><b>Name:</b>{name}</p>
+            <p><b>Age:</b>{age}</p>
+            <p><b>Course:</b>{course}</p>
+            <p><b>City:</b>{city}</p>
+        </div>
+    )
+}
+export default Details;
